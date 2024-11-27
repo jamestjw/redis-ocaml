6890a1dd72ca0fcaf3a6d17504b4f3df290e63e1
James Tan <james_tan97@outlook.com>
2024-11-27 02:25:22 -0500

    handle concurrent clients

--- src/main.ml
+++ src/main.ml
@@ -1,42 +1,62 @@
-open Unix
+open Lwt
+open Lwt.Syntax
 
-let req_buffer_size = 50
+let default_backlog = 5 (* max number of concurrent clients *)
+let default_address = "127.0.0.1"
+let default_port = 6379
 
-let mk_server_socket () =
+let handle_message msg =
+  match msg with
+  | "PING" -> Some "+PONG\r\n"
+  | _ -> None
+;;
+
+let rec handle_connection ic oc () =
+  let* msg = Lwt_io.read_line_opt ic in
+  match msg with
+  | Some msg ->
+    (match handle_message msg with
+     | Some reply ->
+       let* _ = Logs_lwt.info (fun m -> m "Received message %s" msg) in
+       Lwt_io.write oc reply >>= handle_connection ic oc
+     | None -> handle_connection ic oc ())
+  | None -> Logs_lwt.info (fun m -> m "Connection closed")
+;;
+
+let accept_connection conn =
+  let fd, _ = conn in
+  let ic = Lwt_io.of_fd ~mode:Lwt_io.Input fd in
+  let oc = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
+  let* () = Logs_lwt.info (fun m -> m "New connection") in
+  Lwt.on_failure (handle_connection ic oc ()) (fun e ->
+    Logs.err (fun m -> m "%s" (Printexc.to_string e)));
+  return ()
+;;
+
+let create_server sock =
+  let rec serve () = Lwt_unix.accept sock >>= accept_connection >>= serve in
+  serve
+;;
+
+let create_server_socket ~address ~port ~backlog =
+  let open Lwt_unix in
   (* Create a TCP server socket *)
-  let server_socket = Unix.(socket PF_INET SOCK_STREAM 0) in
-  Unix.setsockopt server_socket Unix.SO_REUSEADDR true;
-  Unix.bind server_socket (Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 6379));
-  Unix.listen server_socket 1;
-  server_socket
-;;
-
-let listen socket =
-  let client_socket, _ = Unix.accept socket in
-  client_socket
-;;
-
-let client_loop client_socket =
-  let rec inner () =
-    Fmt.pr "Listening...\n";
-    let buffer = Bytes.create req_buffer_size in
-    let bytes_read = Unix.recv client_socket buffer 0 req_buffer_size [] in
-    (* Convert received bytes to a string *)
-    let received_data = Bytes.sub_string buffer 0 bytes_read in
-    Fmt.pr "Received %d bytes:\n %s" bytes_read received_data;
-    let response = "+PONG\r\n" in
-    let _ =
-      Unix.send client_socket (Bytes.of_string response) 0 (String.length response) []
-    in
-    inner ()
-  in
-  inner ()
+  let sock = socket PF_INET SOCK_STREAM 0 in
+  setsockopt sock SO_REUSEADDR true;
+  ignore @@ bind sock (ADDR_INET (Unix.inet_addr_of_string address, port));
+  listen sock backlog;
+  sock
 ;;
 
 let () =
-  let server_socket = mk_server_socket () in
-  let client_socket = listen server_socket in
-  ignore @@ client_loop client_socket;
-  close client_socket;
-  close server_socket
+  let () = Logs.set_reporter (Logs.format_reporter ()) in
+  let () = Logs.set_level (Some Logs.Info) in
+  let server_socket =
+    create_server_socket
+      ~address:default_address
+      ~port:default_port
+      ~backlog:default_backlog
+  in
+  let serve = create_server server_socket in
+  Lwt_main.run @@ serve ()
 ;;
