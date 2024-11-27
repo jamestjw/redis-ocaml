1ddaf6b2cd78ee93f81e09e4473bc48eb6a6f5d1
James Tan <james_tan97@outlook.com>
2024-11-27 00:34:46 -0500

    implement ping pong

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,24 @@
 open Unix
 
+let buffer_size = 50
+
 let () =
   (* Create a TCP server socket *)
-  let server_socket = socket PF_INET SOCK_STREAM 0 in
-  setsockopt server_socket SO_REUSEADDR true;
-  bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
-  listen server_socket 1;
-  (* Uncomment this block to pass the first stage *)
-  let client_socket, _ = accept server_socket in
+  let server_socket = Unix.(socket PF_INET SOCK_STREAM 0) in
+  Unix.setsockopt server_socket Unix.SO_REUSEADDR true;
+  Unix.bind server_socket (Unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", 6379));
+  Unix.listen server_socket 1;
+  let client_socket, _ = Unix.accept server_socket in
+  let buffer = Bytes.create buffer_size in
+  Fmt.pr "Listening...\n";
+  let bytes_read = Unix.recv client_socket buffer 0 buffer_size [] in
+  (* Convert received bytes to a string *)
+  let received_data = Bytes.sub_string buffer 0 bytes_read in
+  Fmt.pr "Received %d bytes:\n %s" bytes_read received_data;
+  let response = "+PONG\r\n" in
+  let _ =
+    Unix.send client_socket (Bytes.of_string response) 0 (String.length response) []
+  in
   close client_socket;
   close server_socket
 ;;
