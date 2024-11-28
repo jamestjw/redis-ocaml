489af8c1d188941e19a78b8c4c2d045f3dd9fb41
James Tan <james_tan97@outlook.com>
2024-11-28 01:45:29 -0500

    implement redis protocol parser and echo cmd

--- src/main.ml
+++ src/main.ml
@@ -4,22 +4,100 @@ open Lwt.Syntax
 let default_backlog = 5 (* max number of concurrent clients *)
 let default_address = "127.0.0.1"
 let default_port = 6379
+let num_args_regex = Str.regexp {|\*\([0-9]+\)|}
+let arg_len_regex = Str.regexp {|\$\([0-9]+\)|}
 
-let handle_message msg =
-  match msg with
-  | "PING" -> Some "+PONG\r\n"
+type 'a parse_result =
+  | Disconnected
+  | InvalidFormat of string
+  | Parsed of 'a
+
+type command =
+  | PING
+  | ECHO of string
+[@@deriving show { with_path = false }]
+
+let args_to_cmd args =
+  let lower_fst = function
+    | [] -> []
+    | x :: xs -> String.lowercase_ascii x :: xs
+  in
+  match lower_fst args with
+  | [ "ping" ] -> Some PING
+  | [ "echo"; arg ] -> Some (ECHO arg)
   | _ -> None
 ;;
 
+let handle_message cmd =
+  match cmd with
+  | PING -> "+PONG\r\n"
+  | ECHO s -> Printf.sprintf "$%d\r\n%s\r\n" (String.length s) s
+;;
+
+(* Polls the input channel for a valid command, if this returns None this
+   means that the connection has been dropped by the client. *)
+let rec get_cmd ic =
+  let parse_len regexp =
+    let* msg = Lwt_io.read_line_opt ic in
+    match msg with
+    | None -> return Disconnected
+    | Some msg ->
+      if Str.string_match regexp msg 0
+      then return @@ Parsed (int_of_string @@ Str.matched_group 1 msg)
+      else return @@ InvalidFormat msg
+  in
+  let parse_args num_args =
+    let rec parse_args' num_args acc =
+      if num_args = 0
+      then return @@ Parsed (List.rev acc)
+      else
+        let* arg_len = parse_len arg_len_regex in
+        match arg_len with
+        | Disconnected -> return Disconnected
+        | InvalidFormat s ->
+          let* _ = Logs_lwt.err (fun m -> m "Received malformed length %s" s) in
+          return (InvalidFormat s)
+        | Parsed arg_len ->
+          let* msg = Lwt_io.read_line_opt ic in
+          (match msg with
+           | None -> return Disconnected
+           | Some arg when String.length arg <> arg_len ->
+             let* _ =
+               Logs_lwt.err (fun m -> m "Argument (%s) length != %d" arg arg_len)
+             in
+             return @@ InvalidFormat arg
+           | Some arg -> parse_args' (num_args - 1) (arg :: acc))
+    in
+    parse_args' num_args []
+  in
+  let* num_args = parse_len num_args_regex in
+  match num_args with
+  | Disconnected -> return None
+  | InvalidFormat s ->
+    let* _ = Logs_lwt.err (fun m -> m "Received malformed length %s" s) in
+    get_cmd ic
+  | Parsed num_args when num_args <= 0 -> get_cmd ic
+  | Parsed num_args ->
+    let* arg_list = parse_args num_args in
+    (match arg_list with
+     | Disconnected -> return None
+     | InvalidFormat _ -> get_cmd ic
+     | Parsed arg_list ->
+       (match args_to_cmd arg_list with
+        | None ->
+          let* _ =
+            Logs_lwt.err (fun m -> m "Unknown command %s" @@ String.concat "  " arg_list)
+          in
+          return None
+        | Some cmd -> return @@ Some cmd))
+;;
+
 let rec handle_connection ic oc () =
-  let* msg = Lwt_io.read_line_opt ic in
-  match msg with
-  | Some msg ->
-    (match handle_message msg with
-     | Some reply ->
-       let* _ = Logs_lwt.info (fun m -> m "Received message %s" msg) in
-       Lwt_io.write oc reply >>= handle_connection ic oc
-     | None -> handle_connection ic oc ())
+  let* cmd = get_cmd ic in
+  match cmd with
+  | Some cmd ->
+    let* _ = Logs_lwt.info (fun m -> m "Received command %s" @@ show_command cmd) in
+    Lwt_io.write oc (handle_message cmd) >>= handle_connection ic oc
   | None -> Logs_lwt.info (fun m -> m "Connection closed")
 ;;
 
