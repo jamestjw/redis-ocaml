0e1ea07ed267beb3d3f8270ea683364bd505e179
James Tan <james_tan97@outlook.com>
2024-12-06 23:16:36 -0500

    Use Github actions to run CI (#2)
    * Use locks to ensure that tests run sequentially
    
    * Create build.yml for github workflow
    
    * Run ci for all branches and freeze ocamlformat version
    
    * Use printf instead of echo in test

--- test/kill_redis.sh
+++ test/kill_redis.sh
@@ -4,7 +4,6 @@ REDIS_PORT=6379
 
 result=$(netstat -lnp 2>/dev/null | grep $REDIS_PORT | awk '{ print $7 }' | awk -F'/' '{print $1}')
 
-# Check if result is not empty
 if [ ! -z "$result" ]; then
   kill -9 $result 2>/dev/null
 fi
