6405a71f00d68938351af226c01f40f5d0c1821d
James Tan <james_tan97@outlook.com>
2024-12-06 22:08:54 -0500

    use locks to ensure that tests run sequentially

--- test/run_redis.sh
+++ test/run_redis.sh
@@ -3,11 +3,8 @@
 REDIS_PORT=6379
 
 while true; do
-  # Your command here
   result=$(netstat -lnp 2> /dev/null | grep $REDIS_PORT)
-  echo $result
 
-  # Check if result is not empty
   if [ -z "$result" ]; then
     nohup redis_ocaml_server > /dev/null 2>&1 &
     sleep 0.5 # to be sure that server is running
