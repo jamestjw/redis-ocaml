6683d78c1115437e33fee26be97cf59a78728581
James Tan <james_tan97@outlook.com>
2024-12-06 23:34:36 -0500

    add test for reading from rdb file

--- test/run_redis.sh
+++ test/run_redis.sh
@@ -6,7 +6,7 @@ while true; do
   result=$(netstat -lnp 2> /dev/null | grep $REDIS_PORT)
 
   if [ -z "$result" ]; then
-    nohup redis_ocaml_server > /dev/null 2>&1 &
+    nohup redis_ocaml_server "$@" > /dev/null 2>&1 &
     sleep 0.5 # to be sure that server is running
     break
   fi
