82109792b533d49d3b7d5771dc07216e350d643f
James Tan <james_tan97@outlook.com>
2024-12-07 23:47:35 -0500

    check if server is active after executing redis

--- test/run_redis.sh
+++ test/run_redis.sh
@@ -2,15 +2,14 @@
 
 REDIS_PORT=6379
 
-while true; do
-  result=$(netstat -lnp 2> /dev/null | grep $REDIS_PORT)
-
-  if [ -z "$result" ]; then
-    nohup redis_ocaml_server "$@" > /dev/null 2>&1 &
-    sleep 0.5 # to be sure that server is running
-    break
-  fi
-
+# Loop until port is not active so we can launch our instance of Redis
+while [ ! -z "$(netstat -lnp 2>/dev/null | grep $REDIS_PORT)" ]; do
   # Sleep between iterations to avoid overwhelming the system
   sleep 0.2
+done
+
+nohup redis_ocaml_server "$@" >/dev/null 2>&1 &
+
+while [ -z "$(netstat -lnp 2>/dev/null | grep $REDIS_PORT)" ]; do
+  sleep 0.1 # Wait until server is ready before returning
 done
